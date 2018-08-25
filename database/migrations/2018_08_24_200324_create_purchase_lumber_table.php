526fecf3aa184821bb883d1a333f11e8b2d44002
Vrian7 <i-am-brian@hotmail.com>
2018-08-25 12:24:06 -0400

    base project

---
+++ database/migrations/2018_08_24_200324_create_purchase_lumber_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchaseLumberTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_lumber', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('purchase_id')->nullable();
+            $table->foreign('purchase_id')->references('id')->on('purchases');
+            $table->integer('lumber_id')->nullable();
+            $table->foreign('lumber_id')->references('id')->on('lumber');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_lumber');
+    }
+}
---
+++ database/migrations/2018_08_24_200325_create_lumber_inventories_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLumberInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lumber_inventories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('minimum')->default(0);
+            $table->integer('maximum')->default(0);
+            $table->integer('average')->default(0);
+            $table->integer('lumber_id')->nullable();
+            $table->foreign('lumber_id')->references('id')->on('lumber');
+            $table->integer('quantity')->default(0);
+            $table->decimal('price',4,5)->deault(0.00000);            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lumber_inventories');
+    }
+}
