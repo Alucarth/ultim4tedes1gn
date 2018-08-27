3732abe6eb971c3a18729db5516b3478d28d96fb
Vrian7 <i-am-brian@hotmail.com>
2018-08-27 19:59:45 -0400

    provider factory

--- database/factories/ProviderFactory.php
+++ database/factories/ProviderFactory.php
@@ -2,8 +2,12 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Provider::class, function (Faker $faker) {
+    $faker->addProvider(new \Faker\Provider\en_US\Company($faker));
+    $faker->addProvider(new \Faker\Provider\en_US\Person($faker));
     return [
-        //
+        'code'  =>  $faker->randomNumber(8, true),
+        'name'  =>  $faker->lastname.' '.$faker->firstname('male'),
+        'origin'    =>  $faker->unique()->company
     ];
 });
