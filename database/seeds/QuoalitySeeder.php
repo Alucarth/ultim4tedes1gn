626b924304e644e7c411b8de8d69f206667af212
Vrian7 <i-am-brian@hotmail.com>
2018-08-27 20:47:21 -0400

    base de seeders

---
+++ database/seeds/QuoalitySeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class QuoalitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
