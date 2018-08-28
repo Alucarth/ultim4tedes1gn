74a244166f85253dee92f41b8340beca9851a0bf
Vrian7 <i-am-brian@hotmail.com>
2018-08-27 20:29:07 -0400

    specie factory

--- database/factories/QualityFactory.php
+++ database/factories/QualityFactory.php
@@ -6,10 +6,11 @@ $factory->define(Model::class, function (Faker $faker) {
 
     $faker->addProvider(new \Faker\Provider\Miscellaneous($faker));
     $faker->addProvider(new \Faker\Provider\Lorem($faker));
+    $faker->addProvider(new \Faker\Provider\Base($faker));
 
     return [
         'code'  =>  $faker->randomNumber(8, true),
-        'name'  =>  $faker->countryCode,
+        'name'  =>  $faker->randomLetter,
         'description'   =>  $faker->text($maxNbChars = 50)  
     ];
 });
--- database/factories/SpecieFactory.php
+++ database/factories/SpecieFactory.php
@@ -3,7 +3,12 @@
 use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
+
+    $faker->addProvider(new \Faker\Provider\Internet($faker));
+
     return [
-        //
+        'code'  =>  $faker->randomNumber(8, true),
+        'name'  =>  $faker->userName,
+        'description'   =>  $faker->text($maxNbChars = 50)  
     ];
 });
