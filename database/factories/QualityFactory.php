a405e29c0640751994dd9bfa10d118c58e7ab74f
Vrian7 <i-am-brian@hotmail.com>
2018-08-27 20:22:32 -0400

    quality factory

--- database/factories/QualityFactory.php
+++ database/factories/QualityFactory.php
@@ -3,7 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(Model::class, function (Faker $faker) {
+
+    $faker->addProvider(new \Faker\Provider\Miscellaneous($faker));
+    $faker->addProvider(new \Faker\Provider\Lorem($faker));
+
     return [
-        //
+        'code'  =>  $faker->randomNumber(8, true),
+        'name'  =>  $faker->countryCode,
+        'description'   =>  $faker->text($maxNbChars = 50)  
     ];
 });
