70c3dcafbc3792c630d2554106d59b8766b8d849
vrian7 <brian.barrera.bo@gmail.com>
2018-09-02 12:16:13 -0400

    lumber and packages fixes

--- database/migrations/2018_09_01_125450_create_packages_table.php
+++ database/migrations/2018_09_01_125450_create_packages_table.php
@@ -15,8 +15,11 @@ class CreatePackagesTable extends Migration
     {
         Schema::create('packages', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('code');
-            $table->string('name')->nullable();            
+            $table->string('code')->unique();
+            $table->string('name')->nullable();   
+            $table->integer('quantity')->default(0);  //cantidad
+            $table->integer('storage_id');
+            $table->foreign('storage_id')->references('id')->on('storages');
             $table->timestamps();
             $table->softDeletes();
         });
