75fbb72bd50b3f154edc78d3249da62de432e9ea
David Torrez Salinas <ltorrezs2008@gmail.com>
2018-09-09 14:31:06 -0400

    instalando jwt

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,11 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/getLumberData', 'LumberController@getData')->name('lumber_index');
 Route::resource('lumber','LumberController');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'auth'], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
 });
