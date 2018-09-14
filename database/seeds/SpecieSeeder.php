50044ba0e016bdba3f412a65c9f238d5edfe826a
mgisbert <mickygisbert@gmail.com>
2018-09-13 20:58:51 -0400

    seeders

--- database/seeds/SpecieSeeder.php
+++ database/seeds/SpecieSeeder.php
@@ -11,6 +11,31 @@ class SpecieSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\Specie::class, 9)->create();    
+    	DB::statement('TRUNCATE TABLE species RESTART IDENTITY CASCADE');
+    	//DB::table('species')->truncate();
+
+
+        DB::table('species')->insert(['name' => 'Bibosi',]);
+        DB::table('species')->insert(['name' => 'Bitumbo',]);
+        DB::table('species')->insert(['name' => 'Cambara',]);
+        DB::table('species')->insert(['name' => 'Cedro',]);
+        DB::table('species')->insert(['name' => 'Cumaru',]);
+        DB::table('species')->insert(['name' => 'Garapa',]);
+        DB::table('species')->insert(['name' => 'Ipe Champagne',]);
+        DB::table('species')->insert(['name' => 'Mani',]);
+        DB::table('species')->insert(['name' => 'Mara Africano',]);
+        DB::table('species')->insert(['name' => 'Mara Macho',]);
+        DB::table('species')->insert(['name' => 'Marfil',]);
+        DB::table('species')->insert(['name' => 'Morado',]);
+        DB::table('species')->insert(['name' => 'Murure',]);
+        DB::table('species')->insert(['name' => 'Ochoo',]);
+        DB::table('species')->insert(['name' => 'Paquio',]);
+		DB::table('species')->insert(['name' => 'Roble',]);
+		DB::table('species')->insert(['name' => 'Tajibo',]);
+		DB::table('species')->insert(['name' => 'Tajibo Colorado',]);
+		DB::table('species')->insert(['name' => 'Tamarindo',]);
+		DB::table('species')->insert(['name' => 'Varias Especies',]);
+		DB::table('species')->insert(['name' => 'Yesquero Blanco',]);
+		DB::table('species')->insert(['name' => 'Yesquero Negro',]);
     }
 }
