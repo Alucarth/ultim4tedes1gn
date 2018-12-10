3b9b95ef2339db758b38fba5cd433494bfc5f664
Vrian7 <i-am-brian@hotmail.com>
2018-12-10 14:04:23 -0400

    crud insumos

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -115,7 +115,13 @@ class InventoryController extends Controller
      */
     public function show($id)
     {
-        //
+        $inventory = Inventory::with(['family','type','unit'])->find($id);
+        
+        $data = [
+            'inventory' => $inventory
+        ];
+
+        return response()->json($data);
     }
 
     /**
@@ -126,7 +132,13 @@ class InventoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $inventory = Inventory::with(['family','type','unit'])->find($id);
+        
+        $data = [
+            'inventory' => $inventory
+        ];
+
+        return response()->json($data);
     }
 
     /**
@@ -138,7 +150,22 @@ class InventoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $inventory = Inventory::find($id);
+        $inventory->code = $request->code;
+        $inventory->minimum = $request->minimum;
+        $inventory->description = $request->description;
+        $inventory->active = $request->active;
+        $inventory->inventory_type_id = $request->inventory_type_id;
+        $inventory->family_id = $request->family_id;
+        $inventory->unit_id = $request->unit_id;
+        $inventory->observation = "";
+        $inventory->save();
+
+        $data = [
+            'inventory' => $inventory
+        ];
+
+        return response()->json($data);
     }
 
     /**
